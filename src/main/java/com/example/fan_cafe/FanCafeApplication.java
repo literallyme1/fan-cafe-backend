c970d901c9f5a832896a9f39278a0c592e1669ab
literallyme1 <kde7891@naver.com>
2025-06-17 10:21:45 +0900

    Initial commit

---
+++ src/main/java/com/example/fan_cafe/FanCafeApplication.java
@@ -0,0 +1,13 @@
+package com.example.fan_cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FanCafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fan_cafe/FanCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fan_cafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FanCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
