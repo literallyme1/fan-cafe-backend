b44318ceb1a7fced017c40fd1fddc5d756387c9d
literallyme1 <kde7891@naver.com>
2025-07-02 07:59:34 +0900

    where-user-not-deleted

--- src/main/java/com/example/fan_cafe/user/infrastructure/UserRepository.java
+++ src/main/java/com/example/fan_cafe/user/infrastructure/UserRepository.java
@@ -1,11 +1,14 @@
 package com.example.fan_cafe.user.infrastructure;
 
 import com.example.fan_cafe.user.domain.User;
+import org.hibernate.annotations.SQLRestriction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+
+@SQLRestriction("deleted_at IS NULL")
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
