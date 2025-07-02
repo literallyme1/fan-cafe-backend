980622c56b30dfea538bd4ca7fa97192c87ef7e0
literallyme1 <kde7891@naver.com>
2025-07-03 08:03:42 +0900

    comment-entity

---
+++ src/main/java/com/example/fan_cafe/comment/domain/Comment.java
@@ -0,0 +1,38 @@
+package com.example.fan_cafe.comment.domain;
+
+import com.example.fan_cafe.global.common.BaseTimeEntity;
+import com.example.fan_cafe.post.domain.Post;
+import com.example.fan_cafe.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+@Table(name = "comments")
+public class Comment extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_comment_id")
+    private Comment parentComment;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+}
