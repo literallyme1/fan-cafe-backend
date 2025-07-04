7c11d33e71308d3b50966a4baf52815e0f9d7042
literallyme1 <kde7891@naver.com>
2025-07-04 09:40:16 +0900

    entityGraph-n+1

--- src/main/java/com/example/fan_cafe/comment/infrastructure/CommentRepository.java
+++ src/main/java/com/example/fan_cafe/comment/infrastructure/CommentRepository.java
@@ -4,6 +4,7 @@ import com.example.fan_cafe.comment.domain.Comment;
 import lombok.NonNull;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,9 +15,12 @@ import java.util.Optional;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     @NonNull
+    @EntityGraph(attributePaths = {"user", "parent"})
     Optional<Comment> findById(Long id);
 
+    @EntityGraph(attributePaths = {"user", "parent"})
     Slice<Comment> findByPostIdAndParentIsNull(Long postId,Pageable pageable);
 
+    @EntityGraph(attributePaths = {"user", "parent"})
     List<Comment> findByParentIdIn(List<Long> parentIds);
 }
