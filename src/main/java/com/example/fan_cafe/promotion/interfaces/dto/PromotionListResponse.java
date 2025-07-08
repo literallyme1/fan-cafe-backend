1d9b37a1767808e084e70cf8370097900c9d2b13
literallyme1 <kde7891@naver.com>
2025-07-08 15:08:15 +0900

    promotion-dto

--- src/main/java/com/example/fan_cafe/promotion/interfaces/dto/PromotionListResponse.java
+++ src/main/java/com/example/fan_cafe/promotion/interfaces/dto/PromotionListResponse.java
@@ -1,4 +1,19 @@
 package com.example.fan_cafe.promotion.interfaces.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Builder
+@Getter
 public class PromotionListResponse {
+
+    private List<PromotionResponse> promotions;
+
+    public static PromotionListResponse of(List<PromotionResponse> promotions) {
+        return PromotionListResponse.builder()
+                .promotions(promotions)
+                .build();
+    }
 }
--- src/main/java/com/example/fan_cafe/promotion/interfaces/dto/PromotionResponse.java
+++ src/main/java/com/example/fan_cafe/promotion/interfaces/dto/PromotionResponse.java
@@ -1,4 +1,30 @@
 package com.example.fan_cafe.promotion.interfaces.dto;
 
+
+import com.example.fan_cafe.promotion.domain.Promotion;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Builder
+@Getter
 public class PromotionResponse {
+    private Long id;
+    private String title;
+    private String description;
+    private String imageUrl;
+    private LocalDate startAt;
+    private LocalDate endAt;
+
+    public static PromotionResponse from(Promotion promotion) {
+        return PromotionResponse.builder()
+                .id(promotion.getId())
+                .title(promotion.getTitle())
+                .description(promotion.getDescription())
+                .imageUrl(promotion.getImageUrl())
+                .startAt(promotion.getStartAt())
+                .endAt(promotion.getEndAt())
+                .build();
+    }
 }
