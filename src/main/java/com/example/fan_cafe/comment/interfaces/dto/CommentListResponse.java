ebe51c912eeed030b5e560042f04ac04f67fcc54
literallyme1 <kde7891@naver.com>
2025-10-22 21:30:52 +0900

    add-cursor-to-comment-list-response

--- src/main/java/com/example/fan_cafe/comment/interfaces/dto/CommentListResponse.java
+++ src/main/java/com/example/fan_cafe/comment/interfaces/dto/CommentListResponse.java
@@ -1,5 +1,6 @@
 package com.example.fan_cafe.comment.interfaces.dto;
 
+import com.example.fan_cafe.global.common.Cursor;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,9 +10,10 @@ import java.util.List;
 @Getter
 public class CommentListResponse {
     private List<CommentResponse> comments;
+    private final Cursor nextCursor;
     private boolean hasNext;
 
-    public static CommentListResponse from(List<CommentResponse> comments, boolean hasNext) {
+    public static CommentListResponse from(List<CommentResponse> comments, Cursor nextCursor, boolean hasNext) {
         return CommentListResponse.builder()
                 .comments(comments)
                 .hasNext(hasNext)
