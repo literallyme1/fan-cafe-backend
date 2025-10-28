918cdbe957314ae2a410b4d7efe29459f432c46e
literallyme1 <kde7891@naver.com>
2025-10-28 18:37:08 +0900

    step4:like-repository-updated

--- src/main/java/com/example/fan_cafe/like/infrastructure/LikeRepository.java
+++ src/main/java/com/example/fan_cafe/like/infrastructure/LikeRepository.java
@@ -1,15 +1,19 @@
 package com.example.fan_cafe.like.infrastructure;
 
 import com.example.fan_cafe.like.domain.Like;
-import com.example.fan_cafe.post.domain.Post;
+
+import com.example.fan_cafe.like.domain.LikeTargetType;
 import com.example.fan_cafe.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 import java.util.Optional;
 
 public interface LikeRepository extends JpaRepository<Like, Long>, LikeRepositoryCustom  {
 
-    List<Like> findByUser(User user);
-    Optional<Like> findByUserAndPost(User user, Post post);
+    //사용자의 like 여부 확인
+    //TODO : User FK 를 지운 후 User 대신 UserId 로 검색
+    Optional<Like> findByTargetIdAndTargetTypeAndUserId(
+            Long targetId,
+            LikeTargetType targetType,
+            User user
+    );
 }
