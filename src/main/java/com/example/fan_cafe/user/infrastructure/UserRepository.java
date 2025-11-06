70d7d6d0a9bf4364cdaf5ca6813231a6bfc69d37
literallyme1 <kde7891@naver.com>
2025-11-06 13:27:13 +0900

    step6-add-method-user-repository

--- src/main/java/com/example/fan_cafe/user/infrastructure/UserRepository.java
+++ src/main/java/com/example/fan_cafe/user/infrastructure/UserRepository.java
@@ -4,6 +4,7 @@ import com.example.fan_cafe.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -15,6 +16,8 @@ public interface UserRepository extends JpaRepository<User, Long>, UserQueryRepo
     boolean existsByEmailAndDeletedAtIsNotNull(String email);
 
     boolean existsByNicknameAndDeletedAtIsNull(String nickname);
+
+    List<User> findAllByIdIn(List<Long> ids);
 
     Optional<User> findByEmailAndDeletedAtIsNull(String email);
 
