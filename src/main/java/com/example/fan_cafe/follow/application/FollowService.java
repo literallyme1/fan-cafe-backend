4ca380f3109bda440eac8530c0bd4b855f128192
literallyme1 <kde7891@naver.com>
2025-11-06 13:58:10 +0900

    step7

--- src/main/java/com/example/fan_cafe/follow/application/FollowService.java
+++ src/main/java/com/example/fan_cafe/follow/application/FollowService.java
@@ -8,6 +8,9 @@ import com.example.fan_cafe.follow.infrastructure.FollowRepository;
 import com.example.fan_cafe.follow.interfaces.dto.FollowerItemResponse;
 import com.example.fan_cafe.follow.interfaces.dto.FollowerListResponse;
 import com.example.fan_cafe.global.common.Cursor;
+import com.example.fan_cafe.global.exception.CustomException;
+import com.example.fan_cafe.user.domain.User;
+import com.example.fan_cafe.user.exception.UserErrorCode;
 import com.example.fan_cafe.user.infrastructure.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,19 +24,34 @@ import java.util.List;
 public class FollowService {
     private final FollowRepository followRepository;
     private final UserRepository userRepository;
+    private final FollowPolicy followPolicy;
+
     @Transactional
-    public void follow(Long followerId, Long targetId){
+    public void follow(Long followerId, Long followingId){
 
-        //user 가 실제 존재하는 지 확인
+        //user 가 실제 존재하는 지 확인 후 User 를 가져옴.
+        long userCount = userRepository.countByIdIn(List.of(followerId, followingId));
+        if(userCount < 2) { throw new CustomException(UserErrorCode.USER_NOT_FOUND);}
 
-//        Follow entity = Follow.create(followerId, targetId);
-//        followRepository.save(entity);
+        //Follow 타당성 여부 확인
+        followPolicy.validate(followerId, followingId);
+
+        User follower = userRepository.getReferenceById(followerId);
+        User following = userRepository.getReferenceById(followingId);
+        Follow entity = Follow.create(follower, following);
+
+        //각 followCount 를 증가시킨다.
+        entity.getFollower().increaseFollowingCount(); //내가 following 함
+        entity.getFollowing().increaseFollowerCount();
+
+        //저장
+        followRepository.save(entity);
+
+
         // TODO: 이벤트 발행 (알림/피드)
     }
 
-    private boolean isValidateUser(Long followerId, Long targetId){
 
-    }
     @Transactional
     public void unfollow(Long followerId, Long targetId) {
         followRepository.deleteById(new FollowId(followerId, targetId));
