85d57ee06f5216eac80ad7cfe66ab51ae896bccb
literallyme1 <kde7891@naver.com>
2025-11-06 12:49:27 +0900

    step6:add-adapter

--- src/main/java/com/example/fan_cafe/follow/infrastructure/adapter/FollowExistsAdapter.java
+++ src/main/java/com/example/fan_cafe/follow/infrastructure/adapter/FollowExistsAdapter.java
@@ -1,7 +1,17 @@
 package com.example.fan_cafe.follow.infrastructure.adapter;
 
 
+import com.example.fan_cafe.follow.domain.FollowPolicy;
+import com.example.fan_cafe.follow.infrastructure.FollowRepository;
+import lombok.RequiredArgsConstructor;
 
-public class FollowExistsAdapter {
+@RequiredArgsConstructor
+public class FollowExistsAdapter implements FollowPolicy.ExistsFollowPort {
+
+    private final FollowRepository followRepository;
+    @Override
+    public boolean exists(Long followerId, Long followingId){
+        return followRepository.existsByFollowerIdAndFollowingId(followerId, followingId);
+    }
 
 }
