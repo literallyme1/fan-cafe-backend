cdd171c59959b9ed24313d45ccd1f97bc6cd077e
literallyme1 <kde7891@naver.com>
2025-11-11 09:06:07 +0900

    step1:get-func-follow-controller

--- src/main/java/com/example/fan_cafe/follow/interfaces/rest/FollowController.java
+++ src/main/java/com/example/fan_cafe/follow/interfaces/rest/FollowController.java
@@ -3,6 +3,7 @@ package com.example.fan_cafe.follow.interfaces.rest;
 
 import com.example.fan_cafe.follow.application.FollowService;
 import com.example.fan_cafe.follow.interfaces.dto.FollowerListResponse;
+import com.example.fan_cafe.global.common.Cursor;
 import com.example.fan_cafe.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -28,12 +29,19 @@ public class FollowController {
         followService.unfollow(user.getId(), targetId);
     }
 
-    @GetMapping("/{id}/followers")
-    public FollowerListResponse followers(@PathVariable("id") Long targetId,
-                                          @AuthenticationPrincipal(expression = "user") User user,
-                                          @RequestParam(required = false) LocalDateTime cursorAt,
-                                          @RequestParam(required = false) Long cursorId,
-                                          @RequestParam(defaultValue = "20") int size) {
-        return followService.getFollowers(targetId, user.getId(), cursorAt, cursorId, size);
+//    @GetMapping("/{id}/followers")
+//    public FollowerListResponse followers(@PathVariable("id") Long targetId,
+//                                          @AuthenticationPrincipal(expression = "user") User user,
+//                                          @RequestParam(required = false) LocalDateTime cursorAt,
+//                                          @RequestParam(required = false) Long cursorId,
+//                                          @RequestParam(defaultValue = "20") int size) {
+//        return followService.getFollowers(targetId, user.getId(), cursorAt, cursorId, size);
+//    }
+
+    @GetMapping("/{userId}/following")
+    public FollowerListResponse getFollowing(@PathVariable("userId") Long userId,
+                             @RequestParam(required = false)Cursor cursor,
+                             @RequestParam(defaultValue = "20") int size){
+        return followService.getFollowing(userId, cursor, size);
     }
 }
