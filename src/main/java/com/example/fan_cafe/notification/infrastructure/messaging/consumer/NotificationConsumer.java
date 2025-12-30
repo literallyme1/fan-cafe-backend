48245b77502cffe7b51dd2dadeb53ddcb48ca4eb
literallyme1 <kde7891@naver.com>
2025-12-30 11:11:35 +0900

    retry-router

--- src/main/java/com/example/fan_cafe/notification/infrastructure/messaging/consumer/NotificationConsumer.java
+++ src/main/java/com/example/fan_cafe/notification/infrastructure/messaging/consumer/NotificationConsumer.java
@@ -1,4 +1,17 @@
 package com.example.fan_cafe.notification.infrastructure.messaging.consumer;
 
+import com.example.fan_cafe.notification.application.retry.RetryPolicy;
+import com.example.fan_cafe.notification.infrastructure.messaging.retry.RetryRouter;
+import com.example.fan_cafe.notification.infrastructure.messaging.retry.XDeathHeaderReader;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class NotificationConsumer {
+
+    private final RetryPolicy retryPolicy;
+    private final XDeathHeaderReader xDeathHeaderReader;
+    private final RetryRouter retryRouter;
+
 }
--- src/main/java/com/example/fan_cafe/notification/infrastructure/messaging/retry/RetryRouter.java
+++ src/main/java/com/example/fan_cafe/notification/infrastructure/messaging/retry/RetryRouter.java
@@ -1,4 +1,30 @@
 package com.example.fan_cafe.notification.infrastructure.messaging.retry;
 
+import com.example.fan_cafe.notification.application.retry.RetryTarget;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+import static com.example.fan_cafe.notification.infrastructure.messaging.NotificationMqNames.*;
+
+@Component
+@RequiredArgsConstructor
 public class RetryRouter {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    //결정 대상을 큐로 보낸다.
+    public void routeRetry(Message message, RetryTarget target) {
+        switch (target) {
+            case RETRY_5S ->
+                rabbitTemplate.send(RETRY_EXCHANGE, RETRY_5S_ROUTING_KEY, message); //5초 큐
+
+            case RETRY_30S ->
+                rabbitTemplate.send(RETRY_EXCHANGE, RETRY_30S_ROUTING_KEY, message); //30초 큐
+
+            case DLQ ->
+                rabbitTemplate.send(DLQ_EXCHANGE,  DLQ_ROUTING_KEY, message);
+        }
+    }
 }
