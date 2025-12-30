b97e3c66c15e62ac9691642a37cd1ce38d80a05c
literallyme1 <kde7891@naver.com>
2025-12-30 16:18:27 +0900

    notification-consumer

--- src/main/java/com/example/fan_cafe/notification/infrastructure/messaging/NotificationMqNames.java
+++ src/main/java/com/example/fan_cafe/notification/infrastructure/messaging/NotificationMqNames.java
@@ -14,6 +14,11 @@ public final class NotificationMqNames {
     public static final String RETRY_5S_ROUTING_KEY = "notif.retry.5s";
     public static final String RETRY_30S_ROUTING_KEY = "notif.retry.30s";
 
+    // DLQ
+    public static final String DLQ_EXCHANGE = "notif.dlq.ex"; //어디로 버릴지
+    public static final String DLQ_QUEUE = "notif.dlq.q"; //어디에 쌓을 지
+    public static final String DLQ_ROUTING_KEY = "notif.dlq"; //규칙
+
     // TTL
     public static final int RETRY_5S_TTL_MS = 5_000;
     public static final int RETRY_30S_TTL_MS = 30_000;
--- src/main/java/com/example/fan_cafe/notification/infrastructure/messaging/consumer/NotificationConsumer.java
+++ src/main/java/com/example/fan_cafe/notification/infrastructure/messaging/consumer/NotificationConsumer.java
@@ -1,10 +1,18 @@
 package com.example.fan_cafe.notification.infrastructure.messaging.consumer;
 
 import com.example.fan_cafe.notification.application.retry.RetryPolicy;
+import com.example.fan_cafe.notification.application.retry.RetryTarget;
 import com.example.fan_cafe.notification.infrastructure.messaging.retry.RetryRouter;
 import com.example.fan_cafe.notification.infrastructure.messaging.retry.XDeathHeaderReader;
+import com.rabbitmq.client.Channel;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.core.Message;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+import static com.example.fan_cafe.notification.infrastructure.messaging.NotificationMqNames.MAIN_QUEUE;
 
 @Component
 @RequiredArgsConstructor
@@ -13,5 +21,32 @@ public class NotificationConsumer {
     private final RetryPolicy retryPolicy;
     private final XDeathHeaderReader xDeathHeaderReader;
     private final RetryRouter retryRouter;
+
+    @RabbitListener(queues = MAIN_QUEUE)
+    public void consume(Message message, Channel channel) throws IOException {
+        try {
+//            process(message);
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);
+        } catch(Exception e) {
+            //로직 오류일 시 바로 DLQ
+            if (!retryPolicy.isRetryable(e)) {
+                channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, false); //마지막 bool , 큐에 넣지 말고 버려라
+                return;
+            }
+
+            //지금까지 실패 횟수 확인
+            int retryCount = xDeathHeaderReader.getRetryCount(message);
+
+            //다음 목적지 결정
+            RetryTarget target = retryPolicy.decideRetryTarget(retryCount);
+
+            //직접 재전송
+            retryRouter.routeRetry(message, target);
+
+            // 원래 메시지는 정상 처리된 것으로 ack
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false); //deliveryTag : 메세지 식별자, 하나만 (false)
+
+        }
+    }
 
 }
