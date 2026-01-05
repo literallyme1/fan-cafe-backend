221698042c8bfefe974aaefe23cf3e22155e713d
literallyme1 <kde7891@naver.com>
2026-01-05 11:55:02 +0900

    handleFailure-process-deserialize

--- src/main/java/com/example/fan_cafe/notification/infrastructure/messaging/consumer/NotificationConsumer.java
+++ src/main/java/com/example/fan_cafe/notification/infrastructure/messaging/consumer/NotificationConsumer.java
@@ -7,20 +7,19 @@ import com.example.fan_cafe.notification.infrastructure.messaging.retry.XDeathHe
 import com.rabbitmq.client.Channel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.net.SocketTimeoutException;
 
 import static com.example.fan_cafe.notification.infrastructure.messaging.NotificationMqNames.MAIN_QUEUE;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public class NotificationConsumer {
+public abstract class NotificationConsumer<T> {
 
     private final RetryPolicy retryPolicy;
     private final XDeathHeaderReader xDeathHeaderReader;
@@ -34,40 +33,51 @@ public class NotificationConsumer {
         long tag = message.getMessageProperties().getDeliveryTag();
 
         try {
-//            process(message);
-            channel.basicAck(tag,false);
+            //message -> event
+            T event = deserialize(message);
+            process(event);
+            channel.basicAck(tag, false);
             //이미 처리된 경우 -> ack
         } catch (DataIntegrityViolationException e) {
             channel.basicAck(tag, false);
-        } catch(Exception e) {
-            //지금까지 실패 횟수 확인
-            int retryCount = xDeathHeaderReader.getRetryCount(message);
-
-            //로직 오류일 시 바로 DLQ
-            if (!retryPolicy.isRetryable(e)) {
-                log.error(
-                        "notification sent to DLQ: retryCount={}",
-                        retryCount
-                );
-                channel.basicNack(tag, false, false); //마지막 bool , 큐에 넣지 말고 버려라
-                return;
-            }
-
-            //다음 목적지 결정
-            RetryTarget target = retryPolicy.decideRetryTarget(retryCount);
-
-            log.warn(
-                    "notification retry: retryCount={}, target={}",
-                     retryCount, target
-            );
-
-            //직접 재전송
-            retryRouter.routeRetry(message, target, e);
-
-            // 원래 메시지는 정상 처리된 것으로 ack
-            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false); //deliveryTag : 메세지 식별자, 하나만 (false)
-
+        } catch (Exception e) {
+            handleFailure(message, channel, e);
         }
     }
+
+    private void handleFailure(Message message, Channel channel, Exception e) throws IOException {
+        long tag = message.getMessageProperties().getDeliveryTag();
+        //지금까지 실패 횟수 확인
+        int retryCount = xDeathHeaderReader.getRetryCount(message);
+
+        //로직 오류일 시 바로 DLQ
+        if (!retryPolicy.isRetryable(e)) {
+            log.error(
+                    "notification sent to DLQ: retryCount={}",
+                    retryCount
+            );
+            channel.basicNack(tag, false, false); //마지막 bool , 큐에 넣지 말고 버려라
+            return;
+        }
+
+        //다음 목적지 결정
+        RetryTarget target = retryPolicy.decideRetryTarget(retryCount);
+
+        log.warn(
+                "notification retry: retryCount={}, target={}",
+                retryCount, target
+        );
+
+        //직접 재전송
+        retryRouter.routeRetry(message, target, e);
+
+        // 원래 메시지는 정상 처리된 것으로 ack
+        channel.basicAck(tag, false); //deliveryTag : 메세지 식별자, 하나만 (false)
+    }
+
+    protected abstract T deserialize(Message message);
+
+    protected abstract void process(T event);
+
 
 }
