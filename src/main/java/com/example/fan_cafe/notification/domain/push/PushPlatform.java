5a742f67b7c90dfcb16e1249b8ed1c68e8277683
literallyme1 <kde7891@naver.com>
2026-01-12 18:26:16 +0900

    push-token

---
+++ src/main/java/com/example/fan_cafe/notification/domain/push/PushPlatform.java
@@ -0,0 +1,6 @@
+package com.example.fan_cafe.notification.domain.push;
+
+public enum PushPlatform {
+    ANDROID,
+    IOS
+}
---
+++ src/main/java/com/example/fan_cafe/notification/domain/push/PushToken.java
@@ -0,0 +1,52 @@
+package com.example.fan_cafe.notification.domain.push;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(
+        name = "push_token",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"user_id", "token"})
+        }
+)
+public class PushToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    //FCM 토큰
+    @Column(nullable = false, length = 255)
+    private String token;
+
+    // 디바이스 플랫폼
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PushPlatform platform;
+
+    // 유효 여부 (실패 시 false)
+    @Column(nullable = false)
+    private boolean active = true;
+
+    // 마지막 사용 시점
+    private LocalDateTime lastUsedAt;
+
+
+    public void markUsed() {
+        this.lastUsedAt = LocalDateTime.now();
+    }
+
+    public void deactivate() {
+        this.active = false;
+    }
+}
