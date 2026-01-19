2539dcaf7cacc245ce861e014cc73794334f4d36
literallyme1 <kde7891@naver.com>
2026-01-19 18:57:13 +0900

    local-config-for-health-check

--- src/main/java/com/example/fan_cafe/infrastructure/security/ActuatorSecurityConfig.java
+++ src/main/java/com/example/fan_cafe/infrastructure/security/ActuatorSecurityConfig.java
@@ -3,12 +3,14 @@ package com.example.fan_cafe.infrastructure.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @Profile("prod") // 운영에서만 적용
+@Order(1)
 public class ActuatorSecurityConfig {
 
     @Bean
